2ccd137c1d39434ef44e589cc1cd69eb56e8815e
Gaurav Sali <gauravps18@gmail.com>
2019-02-18 19:01:32 +0530

    feat: added vue-router

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is TodoList app, which is powered by VueJS and Vue-Router.</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Header from '../components/layout/Header.vue';
+import Todos from '../components/Todos.vue';
+import AddTodo from '../components/AddTodo.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => this.todos = this.todos.filter(todo => todo.id != id))
+        .catch(err => console.error(err));
+    },
+    addTodo(todo) {
+      const {title, completed} = todo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.error(err));
+    }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.error(err));
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
